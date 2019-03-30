0fb5f3eb2a001e662e220ef7997e563e713e02cf
18075555 <18075555@cnsuning.com>
2019-03-30 20:19:25 +0800

    springCloud add ribbon test

--- src/main/java/com/sn/springCloud102/ApplicationProviderController.java
+++ src/main/java/com/sn/springCloud102/ApplicationProviderController.java
@@ -31,6 +31,6 @@ public class ApplicationProviderController {
 //负载均衡测试demo
     @GetMapping("/add")
     public String addTest(Integer a, Integer b, HttpServletRequest request){
-        return "Form port:"+request.getServerPort()+",result:"+ a+b;
+        return " Form port:"+request.getServerPort()+",result:"+ a+b;
     }
 }
